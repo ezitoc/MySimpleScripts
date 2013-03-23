b6dddf01908d68a320793990c9f51d8380f1c75a
M. Ezequiel Castillo <ezecastillo@gmail.com>
2013-03-22 23:52:49 -0300

    Update calcE.py

--- Scripts/calcE.py
+++ Scripts/calcE.py
@@ -8,7 +8,7 @@
 #
 #* Creation Date : 11-03-2013
 #
-#* Last Modified : jue 21 mar 2013 21:16:32 ART
+#* Last Modified : Fri 22 Mar 2013 06:46:09 PM ART
 #
 #* Created By :  Ezequiel Castillo
 #
@@ -25,12 +25,49 @@ rc('font',**{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
 
 
+def checkCorr(Xvalues, Yvalues):
+
+    def func(t, a, b, c):
+        """Fitting exponential function"""
+        return a*np.exp(-t/b)+np.exp(-t/c)
+
+    # Ignore overflow error (for curve_fit)
+    np.seterr(over='ignore')
+
+    Aprom = np.average(Yvalues)
+    A = Yvalues-Aprom
+    corr = np.correlate(A, A, "full")
+
+    # Auto Correlation Function
+    ACF = corr[corr.size/2:]/(np.var(A)*len(A))
+    print len(Xvalues)
+
+
+    # Exponential fit with Scipy's curve_fit
+    popt, pcov = curve_fit(func, Xvalues, ACF)
+    print popt, pcov
+
+    # Tau, integration of correlation function
+    tau = popt[0]*popt[1]+popt[2]
+    print tau
+
+    #print popt[0], popt[1], popt[2], popt[3]
+    plt.plot(Xvalues, ACF, 'o', label="ACF")
+    plt.plot(Xvalues, func(Xvalues, *popt), label="Fitted Curve")
+    plt.legend(loc='upper right')
+    plt.xlabel('Tiempo [ps]')
+    plt.ylabel('$C(t)$')
+    plt.axis([0,5*tau,-0.05,1])
+    plt.show()
+    plt.savefig('corr.pdf', format='pdf', bbox_inches='tight')
+
+
 
 class calcE(object):
     """ Super class"""
 
     def __init__(self, vprom=None, alpha=None, vmin=None, Ecut=None, freq=None,
-                 temp=None, biasFile=None, enerFile=None, tempFile=None,
+                 T=None, biasFile=None, enerFile=None, tempFile=None,
                  smoothFactor=None, tau=None):
 
         if Ecut and freq:
@@ -39,15 +76,16 @@ class calcE(object):
             raise NameError('Smooth factor and tau not at the same time!')
 
         self.kB = 8.6173324*10**-5 # eV/K
+        self.T = T # K
+        self.beta = 1./(self.kB*self.T)
         self.vprom = vprom
         self.alpha = alpha
         self.vmin = vmin
         self.freq = freq
-        self.temp = temp # K
         self.biasFile = biasFile
-        #self.deltaBias = np.loadtxt(self.biasFile, usecols=[1])
+        self.VSinBias = np.loadtxt(self.biasFile, usecols=[0])
+        self.deltaBias = np.loadtxt(self.biasFile, usecols=[1])
         #self.VBias = np.loadtxt(self.biasFile, usecols=[2])
-        #self.VSinBias = self.VBias - self.deltaBias
         self.tempFile = tempFile
         self.enerFile = enerFile
         self.enerPot = np.loadtxt(self.enerFile, usecols=[0])
@@ -74,6 +112,14 @@ class calcE(object):
         return self.VbProm
 
     def evalVrepesado(self):
+        """ We want to get <Vb>b"""
+        #VbSN = self.VSinBias * math.exp(self.beta*self.deltaBias)
+        VbSN = np.array([ self.VSinBias[i]*math.exp(self.beta*self.deltaBias[i]) for i in range(len(self.VSinBias))  ])
+        Norm = np.array([math.exp(self.beta*self.deltaBias[i]) for i in range(len(self.VSinBias))])
+        Vbb = np.sum(VbSN)/np.sum(Norm)
+        checkCorr(self.timeNoBoost, self.VSinBias)
+        print Vbb
+        sys.exit()
         plt.plot(self.timeNoBoost, self.enerPot, 'o-g', label='Potencial no boosteado', markersize=1)
         plt.legend()
         plt.show()
@@ -124,40 +170,6 @@ class calcE(object):
 
         return np.array(newX), np.array(newY)
 
-    def checkCorr(self):#, Xvalues=self.timeBoost, Yvalues=self.enerPot):
-
-        def func(t, a, b, c):
-            """Fitting exponential function"""
-            return a*np.exp(-t/b)+np.exp(-t/c)
-
-        # Ignore overflow error (for curve_fit)
-        np.seterr(over='ignore')
-
-        Aprom = np.average(Yvalues)
-        A = Yvalues-Aprom
-        corr = np.correlate(A, A, "full")
-
-        # Auto Correlation Function
-        ACF = corr[corr.size/2:]/(np.var(A)*len(A))
-
-
-        # Exponential fit with Scipy's curve_fit
-        popt, pcov = curve_fit(func, Xvalues, ACF)
-
-        # Tau, integration of correlation function
-        tau = popt[0]*popt[1]+popt[2]
-
-        #print popt[0], popt[1], popt[2], popt[3]
-        plt.plot(t, corr, 'o', label="ACF")
-        plt.plot(t, func(t, *popt), label="Fitted Curve")
-        plt.legend(loc='upper right')
-        plt.xlabel('Tiempo [ps]')
-        plt.ylabel('$C(t)$')
-        plt.axis([0,5*tau,-0.05,1])
-        plt.show()
-        plt.savefig('corr.pdf', format='pdf', bbox_inches='tight')
-
-
 
 if __name__ == "__main__":
     """ A continuacion se definen las variables a utilizar por el programa. """
@@ -168,7 +180,7 @@ if __name__ == "__main__":
               #tempFile='temp.dat', smoothFactor=0.001)
 
     g = calcE(vprom=-1540.45, alpha=0.8, vmin=-1554.63, freq=1*10-3,
-              temp=300, biasFile='bias.dat', enerFile='ener.dat',
+              T=300, biasFile='bias.dat', enerFile='ener.dat',
               tempFile='temp.dat', tau=3.52)
 
     #energy = f.evalE()
