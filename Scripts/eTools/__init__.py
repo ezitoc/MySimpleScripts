9dc4769607cced0c176bbe00693d8357173c7f4e
M. Ezequiel Castillo <ezecastillo@gmail.com>
2013-12-31 22:14:29 -0300

    added CONECTION

--- Scripts/eTools/__init__.py
+++ Scripts/eTools/__init__.py
@@ -21,6 +21,16 @@ atno_to_symbol = \
 
 symbol_to_atno = dict([[v,k] for k,v in atno_to_symbol.items()])
 
+VdW_Radii = \
+        {1: 0.37,
+         2: 0.32,
+         6: 0.77,
+         7: 0.75,
+         8: 0.73,
+        16: 1.02,
+        79: 1.44
+         }
+
 DefaultSiestaKeys = \
     {'SystemName': 'siesta',
     'SystemLabel': 'siesta',
@@ -84,6 +94,61 @@ def is_mono_dim_list(a_list):
             return False
     return True
 
+def get_distance(obj1, obj2):
+    if isinstance(obj1, Atom):
+        pos1 = obj1.pos
+    if isinstance(obj2, Atom):
+        pos2 = obj2.pos
+    return np.linalg.norm(pos1-pos2)
+
+def find_shortest_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if not graph.has_key(start):
+        return None
+    shortest = None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_shortest_path(graph, node, end, path)
+            if newpath:
+                if not shortest or len(newpath) < len(shortest):
+                    shortest = newpath
+    return shortest
+
+def find_shortest_path(graph, start, length, path=[]):
+    path = path + [start]
+    #if start == end:
+    if len(path) == length
+        return path
+    if not graph.has_key(start):
+        return None
+    shortest = None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_shortest_path(graph, node, length-1, path)
+            if newpath:
+                if not shortest or len(newpath) < len(shortest):
+                    shortest = newpath
+    return shortest
+
+#def path_finder(graph, length, path=[]):
+    ##if not graph.has_key(start)
+    ##result = []
+    #for key in graph:
+        #a_path = [start]
+        #if length == 1:
+            #path.append(a_path)
+        #for node in graph[start]:
+            #if node == start:
+                #pass
+
+def get_angle(vec1, vec2):
+    angle = np.arccos(np.inner(vec1, vec2)/
+                      (np.linalg.norm(vec1)*np.linalg.norm(vec2)))
+    return np.rad2deg(angle)
+
+
 class AttrDict(dict):
     """ Class for accessing dict keys like an attribute  """
     def __init__(self, *args, **kwargs):
@@ -100,7 +165,7 @@ class Atom(object):
 
     last_id = count()
 
-    def __init__(self, symbol, pos, siesta_move=(1,1,1)):
+    def __init__(self, symbol, pos, label=None, siesta_move=(1,1,1)):
 
         #self.pos = Point(x, y, z)
         self.set_position(pos)
@@ -108,12 +173,20 @@ class Atom(object):
 
         if re.match(r'[a-zA-Z]+.*', symbol):
             self.symbol = symbol
+            atno = None
         else:
-            self.atno = symbol
+            atno = symbol
         if not self.symbol:
             self.symbol = self.atno_to_symbol()
+        if not atno:
+            atno = self.symbol_to_atno()
+        self.atno = atno
 
         self.index = self.last_id.next()
+        if label:
+            self.label = label
+        else:
+            self.label = str(self.index)
 
     def set_position(self, pos):
         #if pos:
@@ -153,33 +226,46 @@ class Atom(object):
         self.__sub__(an_object)
 
     def __repr__(self):
-        return '{name}({sym!r}, {pos})'.format(
-            name=self.__class__.__name__, sym=self.symbol,
-            pos=self.pos)
+        return '{label}-{sym}'.format(sym=self.symbol, label=self.label)
+        #return '{name}({sym!r}, {pos}, {index})'.format(
+            #name=self.__class__.__name__, sym=self.symbol,
+            #pos=self.pos, index=self.index)
 
 
 class Molecule(object):
 
     last_id = count()
 
-    def __init__(self, name='Generic', atom_list=None):
+    def __init__(self, name='Generic', atoms=None, fromFile=None):
 
         if name == 'Generic':
             self.name = 'Generic' + str(self.last_id.next())
         else:
             self.name = name
 
-        if atom_list:
-            self.atom_list = atom_list
+        if atoms:
+            self.atoms = atoms
         else:
-            self.atom_list = []
+            self.atoms = []
+
+        if fromFile:
+            self.load_from_file(fromFile)
+
+        self.get_conections()
+        self.get_structure()
+
+    def update_mol(self):
+        for i, atom in enumerate(self.atoms):
+            atom.label = str(i)
 
     def add_atom(self, atom):
-        self.atom_list.append(atom)
+        self.atoms.append(atom)
+        self.update_mol()
 
     def remove_atom(self, i):
-        print 'Deleting {atom}'.format(atom=self.atom_list[i])
-        self.atom_list.pop(i)
+        print 'Deleting {atom}'.format(atom=self.atoms[i])
+        self.atoms.pop(i)
+        self.update_mol()
 
     def load_from_file(self, filename, filetype='xyz'):
 
@@ -207,12 +293,14 @@ class Molecule(object):
             posarr = np.array(lines)*toAng
             posarr = posarr.T
 
-        self.atom_list = [Atom(namelst[i], pos) for i, pos in enumerate(posarr)]
+        self.atoms = [Atom(namelst[i], pos) for i, pos in enumerate(posarr)]
+
+        self.update_mol()
 
     def write_to_file(self, filename='default.xyz', fmt='xyz'):
         with open(filename, 'w') as fout:
-            fout.write('{tot_at:5d}\n\n'.format(tot_at=len(self.atom_list)))
-            for atom in self.atom_list:
+            fout.write('{tot_at:5d}\n\n'.format(tot_at=len(self.atoms)))
+            for atom in self.atoms:
                 fout.write('{sym:3s}{x:13.8f}{y:13.8f}{z:13.8f}\n'.format(sym=atom.symbol,
                                                                         x=atom.pos[0],
                                                                         y=atom.pos[1],
@@ -220,20 +308,64 @@ class Molecule(object):
 
     def get_geometry_center(self):
         asum = np.zeros(3)
-        for atom in self.atom_list:
+        for atom in self.atoms:
             asum += atom.pos
-        asum /= len(self.atom_list)
+        asum /= len(self.atoms)
         return asum
 
     def get_species(self):
-        return list(set([atom.symbol for atom in self.atom_list]))
+        return list(set([atom.symbol for atom in self.atoms]))
         #return {i+1:specie for i, specie in enumerate(species)}
         #return OrderedDict({i+1:specie for i, specie in enumerate(species)})
+
+    def get_conections(self):
+        conections = {}
+        for atom in self.atoms:
+            lconections = []
+            ddist = self.get_closest_atom(atom, dist=True)
+            for close_atom in ddist:
+                pair_dist = VdW_Radii[atom.atno] + VdW_Radii[close_atom.atno]
+                if ddist[close_atom] < pair_dist:
+                    lconections.append(close_atom)
+            conections[atom] = lconections
+        self.conections = conections
+        return conections
+
+    def get_structure(self):
+        bonds = []
+        angles = []
+        dihedrals = []
+        not_bonded = []
+        for i, f_atom in enumerate(self.atoms):
+            for s_atom in self.atoms[i+1:]:
+                conect = find_shortest_path(self.conections, f_atom, s_atom)
+                if conect:
+                    if len(conect) == 2:
+                        bonds.append(conect)
+                    elif len(conect) == 3:
+                        angles.append(Angle(conect))
+                    elif len(conect) == 4:
+                        dihedrals.append(Dihedral(conect))
+                    else:
+                        not_bonded.append(conect)
+        self.bonds = bonds
+        self.angles = angles
+        self.dihedrals = dihedrals
+        self.not_bonded = not_bonded
+
+    def get_closest_atom(self, atom, dist=False):
+        rest = self.atoms[:]  # Make a copy.
+        rest.remove(atom)
+        closest_list = {an_atom: get_distance(atom, an_atom) for an_atom in rest}
+        if dist:
+            return closest_list
+        else:
+            return sorted(closest_list)
 
     def __add__(self, an_object):
 
         if isinstance(an_object, (self.__class__, Selection)):
-            #self.atom_list += an_object.atom_list
+            #self.atoms += an_object.atoms
             for atom in an_object:
                 self.add_atom(atom)
 
@@ -241,7 +373,7 @@ class Molecule(object):
             self.add_atom(an_object)
 
         elif isinstance(an_object, (types.ListType, np.array)):
-            for atom in self.atom_list:
+            for atom in self.atoms:
                 atom + an_object
 
         else:
@@ -253,19 +385,19 @@ class Molecule(object):
     def __sub__(self, an_object):
 
         if isinstance(an_object, (self.__class__, Selection)):
-            #self.atom_list += an_object.atom_list
-            for i, orig_at in enumerate(self.atom_list):
+            #self.atoms += an_object.atoms
+            for i, orig_at in enumerate(self.atoms):
                 for remove_at in an_object:
                     if orig_at.index == remove_at.index:
                         self.remove_atom(i)
 
         elif isinstance(an_object, Atom):
-            for i, orig_at in enumerate(self.atom_list):
+            for i, orig_at in enumerate(self.atoms):
                 if orig_at.index == an_object.index:
                     self.remove_atom(i)
 
         elif isinstance(an_object, (types.ListType, np.array)):
-            for atom in self.atom_list:
+            for atom in self.atoms:
                 atom - an_object
 
         else:
@@ -281,60 +413,143 @@ class Molecule(object):
         self.__sub__(an_object)
 
     def __getitem__(self, index):
-        return self.atom_list[index]
-        #return self.atom_list[index-1]
+        return self.atoms[index]
+        #return self.atoms[index-1]
 
     def __len__(self):
-        return len(self.atom_list)
+        return len(self.atoms)
 
     def __repr__(self):
         return '{name}({atoms})'.format(name=self.__class__.__name__,
-                atoms=self.atom_list)
+                atoms=self.atoms)
+
+
+class Bond(object):
+    pass
+
+
+class Angle(object):
+    def __init__(self,*args):
+        if len(args) is 1:
+            if isinstance(args[0], types.ListType):
+                self.atom1, self.atom2, self.atom3 = args[0]
+        elif len(args) is 3:
+            for elem in args:
+                if not isinstance(elem, Atom):
+                    raise TypeError('Not Atom Instance')
+            self.atom1, self.atom2, self.atom3 = args
+        else:
+            raise IOError('A list of three atom or three atoms needed')
+
+        vec1 = self.atom1.pos - self.atom2.pos
+        vec2 = self.atom3.pos - self.atom2.pos
+
+        self.angle = get_angle(vec1, vec2)
+
+    def set_angle(self):
+        pass
+
+    def __add__(self):
+        pass
+
+    def __radd__(self):
+        pass
+
+    def __sub__(self):
+        pass
+
+    def __rsub__(self):
+        pass
+
+    def __repr__(self):
+        return '{name} {at1} {at2} {at3} = {angle}'.format(name=self.__class__.__name__,
+                at1=self.atom1, at2=self.atom2, at3=self.atom3, angle=self.angle)
+
+
+class Dihedral(object):
+    def __init__(self,*args):
+        if len(args) is 1:
+            if isinstance(args[0], types.ListType):
+                self.atom1, self.atom2, self.atom3, self.atom4 = args[0]
+        elif len(args) is 4:
+            for elem in args:
+                if not isinstance(elem, Atom):
+                    raise TypeError('Not Atom Instance')
+            self.atom1, self.atom2, self.atom3, self.atom4 = args
+        else:
+            raise IOError('A list of four atom or four atoms needed')
+
+        vec1 = self.atom1.pos - self.atom2.pos
+        vec2 = self.atom3.pos - self.atom2.pos
+        vec3 = - vec2
+        vec4 = self.atom4.pos - self.atom3.pos
+
+        n1 = np.cross(vec1, vec2)
+        n2 = np.cross(vec3, vec4)
+
+        self.dihedral = get_angle(n1, n2)
+
+    def __add__(self):
+        pass
+
+    def __radd__(self):
+        pass
+
+    def __sub__(self):
+        pass
+
+    def __rsub__(self):
+        pass
+
+    def __repr__(self):
+        return '{name} {at1} {at2} {at3} {at4} = {dihedral}'.format(
+            name=self.__class__.__name__, at1=self.atom1, at2=self.atom2,
+            at3=self.atom3, at4=self.atom4, dihedral=self.dihedral)
 
 
 class Selection(Molecule):
 
     def __init__(self, a_system):
 
-        atom_list = []
+        atoms = []
 
         if isinstance(a_system, Atom):
-            atom_list = a_system
+            atoms = a_system
 
         elif isinstance(a_system, Molecule):
             for atom in a_system:
-                atom_list.append(atom)
+                atoms.append(atom)
 
         elif isinstance(a_system, System):
             for atom in a_system:
-                atom_list.append(atom)
+                atoms.append(atom)
 
-        self.atom_list = atom_list
-        self.orig_alist = atom_list
+        self.atoms = atoms
+        self.orig_alist = atoms
 
     def _filter(self, func):
-        return filter(func, self.atom_list)
+        return filter(func, self.atoms)
 
     def remove_from_list(self, an_element):
-        if an_element in self.atom_list:
-            self.atom_list.remove(an_element)
+        if an_element in self.atoms:
+            self.atoms.remove(an_element)
 
     def by_symbol(self, symbol):
-        self.atom_list = self._filter(lambda a: a.symbol == symbol)
+        self.atoms = self._filter(lambda a: a.symbol == symbol)
         return self
 
     def by_xrange(self, xmin, xmax):
-        self.atom_list = self._filter(lambda a: xmin <= a.pos[0] <=
+        self.atoms = self._filter(lambda a: xmin <= a.pos[0] <=
                                       xmax)
         return self
 
     def by_yrange(self, ymin, ymax):
-        self.atom_list = self._filter(lambda a: ymin <= a.pos[1] <=
+        self.atoms = self._filter(lambda a: ymin <= a.pos[1] <=
                                       ymax)
         return self
 
     def by_zrange(self, zmin, zmax):
-        self.atom_list = self._filter(lambda a: zmin <= a.pos[2] <=
+        self.atoms = self._filter(lambda a: zmin <= a.pos[2] <=
                                       zmax)
         return self
 
@@ -345,17 +560,17 @@ class Selection(Molecule):
             center = center_atom.pos
         else:
             center = np.zeros(3)
-        self.atom_list = self._filter(lambda a: np.linalg.norm(a.pos - center) <=
+        self.atoms = self._filter(lambda a: np.linalg.norm(a.pos - center) <=
                                       radius**2)
         return self
 
     def update_sel(self):
-        for m_atom in self.atom_list:
+        for m_atom in self.atoms:
             for i, o_atom in enumerate(self.orig_alist):
                 if m_atom.index == o_atom.index:
                     self.orig_alist[i] = m_atom
-        self.atom_list = self.orig_alist
-        return self.atom_list
+        self.atoms = self.orig_alist
+        return self.atoms
 
 
 class Siesta(object):
@@ -410,16 +625,16 @@ class Siesta(object):
         else:
             self.blockKeys.LatticeVectors = np.identity(3)*10
 
-    def zmaMolecule(self, mol):
-        zmaMolecule = []
-        for i, atom in enumerate(mol):
-            if i is 0:
-                zmaMolecule.append([atom.specieNo, 0, 0, 0, atom.pos[0],
-                                    atom.pos[1], atom.pos[2],
-                                    atom.siesta_move[0], atom.siesta_move[1],
-                                    atom.siesta_move[2]])
-            if i is 1:
-                zmaMolecule.append([atom.specieNo, 1, 0, 0]<++>)<++>
+    #def zmaMolecule(self, mol):
+        #zmaMolecule = []
+        #for i, atom in enumerate(mol):
+            #if i is 0:
+                #zmaMolecule.append([atom.specieNo, 0, 0, 0, atom.pos[0],
+                                    #atom.pos[1], atom.pos[2],
+                                    #atom.siesta_move[0], atom.siesta_move[1],
+                                    #atom.siesta_move[2]])
+            #if i is 1:
+                #zmaMolecule.append([atom.specieNo, 1, 0, 0]<++>)<++>
 
     def createInput(self):
         inF = open(self.inFile, 'w')
